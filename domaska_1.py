48d9809405b3f65324d259902000af3299282d43
TerenceGo123 <sash.tockarew2018@yandex.ru>
2023-11-05 12:15:00 +0300

    domaska1

---
+++ domaska_1.py
@@ -0,0 +1,42 @@
+
+
+
+
+# Задача D
+
+# x = 'Hello World'
+# первое_слово = x[:x.find(' ')]
+# второе_слово = x[x.find(' '):]
+# print(второе_слово + ' ' + первое_слово)
+
+# Задача E
+
+# x = 'magaaa'
+
+# if x.find('f') < 1:
+#     print(None)
+    
+# elif x.find('f') == x.rfind('f'):
+#     print(x.find('f'))
+
+# else:
+#     x.find('f') != x.rfind('f')
+#     print(x.find('f'), x.rfind('f'))
+
+
+
+
+# Задача F
+ 
+# c = 'duhneaaofiwjxkfkf'
+
+# if c.find('f') < 1:
+#     print(-2)
+# elif print(c.find('f', c.find('f') + 1)):
+
+# Задача G
+# s = '78shahfuiashfafiaw'
+# s = s[:s.find('h')] + s[s.rfind('h') + 1:]
+# print(s)
+
+
